77971afc74b397051e02826d19838fff4875905d
Hao Gao <hao.x.gao@gmail.com>
2013-02-23 00:53:52 -0500

    refactor to use bootstrap and jquery

--- codehere/static/editor.js
+++ codehere/static/editor.js
@@ -4,126 +4,87 @@ var editor = ace.edit("editor");
 
 var selectedlang = "C";
 window.onload = function () {
-    w = window;
-    j = document;
+  var Code = Parse.Object.extend('Code');
 
-    submit = j.getElementById("Submit");
-    output = j.getElementById("output");
+  var code_id = null; 
 
-    submit2 = j.getElementById("Submit2");
-    output2 = j.getElementById("output2");
-
-    var Code = Parse.Object.extend('Code');
-
-    var code_ = null; 
-
-    submit2.onclick = function () {
-      //alert(output2.value);
-      var query = new Parse.Query(Code);
-        query.get(output2.value, {
-          success: function(code) {
-            output.innerHTML = code.get("content")
-            //alert(content);
-          },
-          error: function(code, error) {
-            // The object was not retrieved successfully.
-            // error is a Parse.Error with an error code and description.
-            alert(error.message);
-          }
-        });
-    }
-
-    submit.onclick = function () {
-        if (code_ == null) {
-          code_ = new Code();
+  $("#get").click(function () {
+    var query = new Parse.Query(Code);
+      query.get(output2.value, {
+        success: function(code) {
+          $('#output').html(code.get("content"));
+        },
+        error: function(code, error) {
+          alert(error.message);
         }
-        code_.set('content', editor.getValue());
+      });
+  });
 
-        code_.save(null, {
-          success: function(code) {
-            
-          },
-          error: function(code, error) {
-            // The save failed.
-            // error is a Parse.Error with an error code and description.
-          }
-        });
+  $("#submit").click(function () {
+    var code_obj = new Code();
+    
+    code_obj.set('content', editor.getValue());
 
-        //code.fetch({
-        //  success: function(myObject) {
-            // The object was refreshed successfully.
-        //  },
-        //  error: function(myObject, error) {
-            // The object was not refreshed successfully.
-            // error is a Parse.Error with an error code and description.
-        //  }
-        //})
-        var query = new Parse.Query(Code);
+    code_obj.save(null, {
+      success: function(code) {
+        code_id = code.id;
+        $('#code_id').val(code.id);
+      },
+      error: function(code, error) {
         
-        query.get(code_.id, {
-          success: function(code) {
-            var content = code.get("content");
-            output2.value = code.id;
-          },
-          error: function(code, error) {
-            // The object was not retrieved successfully.
-            // error is a Parse.Error with an error code and description.
-            alert(error.message);
-          }
-        });
-        
-        new Ajax.Request('/codepad/', {
-            method:'post',
-            parameters: {code: editor.getValue(), lang: selectedlang},
-            onSuccess: function(transport) {
-                var response = transport.responseText;
-                output.innerHTML = response;
-
-            },
-            onFailure: function() { alert('Something went wrong...'); }
-        });
-    }
+      }
+    });
+    
+    new Ajax.Request('/codepad/', {
+        method:'post',
+        parameters: {code: editor.getValue(), lang: selectedlang},
+        onSuccess: function(transport) {
+            var response = transport.responseText;
+            $('#output').html(response);
+        },
+        onFailure: function() { alert('Something went wrong...'); }
+    });
+  });
 }
 
 function setlang(value) {
-    selectedlang = value;
-    switch (selectedlang) {
-        case "C":
-        case "C++":
-        editor.getSession().setMode("ace/mode/c_cpp");
-        break;
-        case "D":
-        case "Plain Text":
-        case "Scheme":
-         case "Haskell":
-        editor.getSession().setMode("ace/mode/text");
-        break;
-        case "Lua":
-        editor.getSession().setMode("ace/mode/lua");
-        break;
-        case "OCaml":
-        editor.getSession().setMode("ace/mode/ocaml");
-        break;
-        case "PHP":
-        editor.getSession().setMode("ace/mode/php");
-        break;
-        case "Perl":
-        editor.getSession().setMode("ace/mode/perl");
-        break;
-        case "Python":
-        editor.getSession().setMode("ace/mode/python");
-        break;
-        case "Ruby":
-        editor.getSession().setMode("ace/mode/ruby");
-        break;
-        case "Tcl":
-        editor.getSession().setMode("ace/mode/tcl");
-        break;
-        default:
-        editor.getSession().setMode("ace/mode/text");
-        break;
-
-    }
+  selectedlang = value;
+  switch (selectedlang) {
+    case "C":
+    case "C++":
+    editor.getSession().setMode("ace/mode/c_cpp");
+    break;
+    case "D":
+    case "Plain Text":
+    case "Scheme":
+     case "Haskell":
+    editor.getSession().setMode("ace/mode/text");
+    break;
+    case "Lua":
+    editor.getSession().setMode("ace/mode/lua");
+    break;
+    case "OCaml":
+    editor.getSession().setMode("ace/mode/ocaml");
+    break;
+    case "PHP":
+    editor.getSession().setMode("ace/mode/php");
+    break;
+    case "Perl":
+    editor.getSession().setMode("ace/mode/perl");
+    break;
+    case "Python":
+    editor.getSession().setMode("ace/mode/python");
+    break;
+    case "Ruby":
+    editor.getSession().setMode("ace/mode/ruby");
+    break;
+    case "Tcl":
+    editor.getSession().setMode("ace/mode/tcl");
+    break;
+    default:
+    editor.getSession().setMode("ace/mode/text");
+    break;
+  }
 }
 function hellotemplate() {
     //todo
